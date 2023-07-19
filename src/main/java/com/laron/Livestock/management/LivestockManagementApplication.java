c1b461263d24f5ca23c709d2b32f5cca4b032373
Manuel Lara <smazzbass@gmail.com>
2023-07-19 15:37:06 -0600

    Initial commit

---
+++ src/main/java/com/laron/Livestock/management/LivestockManagementApplication.java
@@ -0,0 +1,13 @@
+package com.laron.Livestock.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivestockManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivestockManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laron/Livestock/management/LivestockManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laron.Livestock.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivestockManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
